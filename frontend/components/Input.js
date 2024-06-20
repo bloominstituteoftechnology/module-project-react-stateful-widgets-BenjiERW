98967f8a22d0225337ad28fbf9ddb5a936e34ce1
Kevin Reeves <167121955+BenjiERW@users.noreply.github.com>
2024-06-20 12:10:14 -0400

    Input.js

--- frontend/components/Input.js
+++ frontend/components/Input.js
@@ -37,16 +37,19 @@ import React from 'react'; /* STEP 0 */
 
 export default function Input() {
   /* STEP 1 */
+  const [inputValue, setInputValue] = useState('')
 
   const changeInput = evt => {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
     const { value } = evt.target;
+    setInputValue(value)
 
     /* STEP 4 */
   };
   const reset = () => {
     /* STEP 5 */
+    setInputValue('')
   };
 
   const style = {

aadd919a5ed8994c59aeb116d402ce16c26944ee
Kevin Reeves <167121955+BenjiERW@users.noreply.github.com>
2024-06-20 12:11:21 -0400

    Moods.js

--- frontend/components/Moods.js
+++ frontend/components/Moods.js
@@ -35,15 +35,19 @@ const sadMood = 'Rather sad';
 
 export default function Moods() {
   /* STEP 1 */
+  const [mood, setMood] = useState(initialMood)
 
   const makeHappy = () => {
     /* STEP 4 */
+    setMood(happyMood)
   };
   const makeSad = () => {
     /* STEP 5 */
+    setMood(sadMood)
   };
   const reset = () => {
     /* STEP 6 */
+    setMood(initialMood)
   };
 
   const style = {
